c849479dcc0ddbb5f498dd07e1ff43c0b4cdfbfc
Ayham <ali1ali2ali123@hotmail.com>
2021-05-29 21:23:49 +0300

    -update

--- hrrPage.php
+++ hrrPage.php
@@ -23,19 +23,32 @@ $fname = $info['fname'];
 $lname = $info['lname'];
 
 $string = "";
+$insert = false;
 
 //all buttons functionality using if
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    echo "wow3";
     if ($_POST['buttontype'] == "mypost") {
-        echo "wow";
         $query = "SELECT j.jid,j.description,j.numOpenings,j.openingdate,j.contract_type,c.name FROM job_posting j 
                   join company c on j.comp_cid = c.cid where hrr_username = '$username'";
         $string = "My Postings<br>";
-    }else
-        echo "wow1";
+    } elseif ($_POST['buttontype'] == "Create Posting") {
+        $query = "insert into job_posting values ({$_POST['jid']},'{$_POST['desc']}',{$_POST['salary']},{$_POST['phone']},{$_POST['open']},'$username','{$_POST['date']}'
+                  ,{$_POST['duration']},{$_POST['cid']},{$_POST['jobtype']},'{$_POST['conttype']}')";
+        $insert = true;
+    }
 
     $result = mysqli_query($conn, $query);
+    if ($insert) {
+        if ($result) {
+            echo "<script>alert('Posting has been created successfully !');
+                   </script>";
+            header("Refresh:0");
+        } else {
+            echo "<script>alert('An error occurred, Either JID is taken or CID does not exist');
+                   </script>";
+            header("Refresh:0");
+        }
+    }else {
 
     //building all data from sql
     if (mysqli_num_rows($result) != 0) {
@@ -47,6 +60,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     } else
         $string .= "<br>No results found";
+}
 }
 
 ?>
@@ -106,7 +120,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <div class="parent" style="margin-top: 10px;flex-direction: column;align-items: center">
     <div style="flex-direction: row"
     <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
-        <button name="buttontype" value="mypost" >Display my<br> Postings</button>
+        <button name="buttontype" value="mypost">Display my<br> Postings</button>
         <span class="vertical"></span>
     </form>
 
@@ -119,81 +133,84 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <form id="newForm" class="form" style="display: none" method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
     <div style="text-align: center;color: red">Please note that all fields are required<br><br></div>
     <label>JID: </label><input type="text" name="jid" style="margin-left: 57px"
-                                    required
-                                    oninvalid="this.setCustomValidity('JID Field is required')"
-                                    oninput="this.setCustomValidity('')">
+                               required
+                               oninvalid="this.setCustomValidity('JID Field is required')"
+                               oninput="this.setCustomValidity('')">
 
     <br>
 
     <label>Description: </label>&hairsp;&hairsp;<input type="text" name="desc"
-                                                     required
-                                                     oninvalid="this.setCustomValidity('Description Field is required')"
-                                                     oninput="this.setCustomValidity('')">
+                                                       required
+                                                       oninvalid="this.setCustomValidity('Description Field is required')"
+                                                       oninput="this.setCustomValidity('')">
     <br>
 
     <label>Salary: </label><input type="number" name="salary" style="margin-left: 37px"
-                                                 required
-                                                 oninvalid="this.setCustomValidity('Salary Field is required')"
-                                                 oninput="this.setCustomValidity('')">
+                                  required
+                                  oninvalid="this.setCustomValidity('Salary Field is required')"
+                                  oninput="this.setCustomValidity('')">
     <br>
 
     <label>Phone: </label><input type="tel" style="margin-left: 36px"
-                                                        name="phone"
-                                                        required
-                                                        oninvalid="this.setCustomValidity('Phone Field is required')"
-                                                        oninput="this.setCustomValidity('')">
+                                 name="phone"
+                                 required
+                                 oninvalid="this.setCustomValidity('Phone Field is required')"
+                                 oninput="this.setCustomValidity('')">
     <br>
 
     <label>Openings: </label><input type="number" name="open" style="margin-left: 14px"
-                                                   required
-                                                   oninvalid="this.setCustomValidity('Openings Field is required')"
-                                                   oninput="this.setCustomValidity('')">
+                                    required
+                                    oninvalid="this.setCustomValidity('Openings Field is required')"
+                                    oninput="this.setCustomValidity('')">
     <br>
 
     <label>Open Date: </label><input type="date" name="date" style="margin-left: 5px;width: 165px"
-                                                    required
-                                                    oninvalid="this.setCustomValidity('Open Date Field is required')"
-                                                    oninput="this.setCustomValidity('')">
+                                     required
+                                     oninvalid="this.setCustomValidity('Open Date Field is required')"
+                                     oninput="this.setCustomValidity('')">
     <br>
 
-    <label>Duration: </label><input type="number" name="username" style="margin-left: 23px"
-                                                   required
-                                                   oninvalid="this.setCustomValidity('Duration Field is required')"
-                                                   oninput="this.setCustomValidity('')">
+    <label>Duration: </label><input type="number" name="duration" style="margin-left: 23px"
+                                    required
+                                    oninvalid="this.setCustomValidity('Duration Field is required')"
+                                    oninput="this.setCustomValidity('')">
     <br>
 
     <div style="text-align: center;margin-bottom: 7px;padding-bottom: 4px">Job Type<br>
         <label>
-            <input type="radio" name="jobtype" id="manager" onchange="disableman()" required>
+            <input type="radio" name="jobtype" id="manager" value="1" onchange="disableman()" required>
             Manager Job</label>
 
         <label>
-            <input type="radio" name="jobtype" id="intern" onchange="disableman()">
+            <input type="radio" name="jobtype" id="intern" value="0" onchange="disableman()">
             Intern Job</label>
     </div>
 
     <div style="text-align: center;margin-bottom: 7px">Contract Type<br>
         <label>
-            <input type="radio" name="conttype" id="ft" required>
+            <input type="radio" name="conttype" id="ft" value="FT" required>
             Full Time</label>
 
         <label>
-            <input type="radio" name="conttype" id="pt">
+            <input type="radio" name="conttype" id="pt" value="PT">
             Part Time</label>
 
         <label>
-            <input type="radio" name="conttype" id="in">
+            <input type="radio" name="conttype" id="in" value="IN" onchange="disablemindays()">
             Internship</label>
     </div>
 
     <label>CID: </label><input type="number" name="cid" style="margin-left: 57px"
-                                              required
-                                              oninvalid="this.setCustomValidity('CID Field is required')"
-                                              oninput="this.setCustomValidity('')">
+                               required
+                               oninvalid="this.setCustomValidity('CID Field is required')"
+                               oninput="this.setCustomValidity('')">
+    <br>
+
+    <label>Internship Min Days: </label><input id="inmin" type="number" name="cid" style="width: 103px" disabled>
     <br>
 
     <div style="text-align: center;">
-        <input id="button" type="submit" value="Create Posting">
+        <input id="button" name="buttontype" type="submit" value="Create Posting">
         <input style="width: 100px;margin-left: 15px" id="button" type="reset" onclick="clear()" value="Clear">
     </div>
     <script>
@@ -216,14 +233,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     function disableman() {
         //get the value if checkbox is checked
         if (document.getElementById("manager").checked) {
-            //enable all the radio button
             document.getElementById("pt").disabled = true;
             document.getElementById("in").disabled = true;
             document.getElementById("ft").checked = true;
-        }else if(document.getElementById("intern").checked){
+        } else if (document.getElementById("intern").checked) {
             document.getElementById("pt").disabled = false;
             document.getElementById("in").disabled = false;
             document.getElementById("ft").checked = false;
+        }
+    }
+
+    function disablemindays() {
+        //get the value if checkbox is checked
+        if (document.getElementById("in").checked) {
+            document.getElementById("inmin").disabled = false;
+        } else if (document.getElementById("pt").checked || document.getElementById("ft").checked) {
+            document.getElementById("inmin").disabled = true;
         }
     }
 </script>
